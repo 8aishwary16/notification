4b9fe2c711b0600094fd06dbf94c8b39a0a29a15
8aishwary16 <8aishwary16@gmail.com>
2015-03-10 20:57:08 +0530

    done

---
+++ app/src/main/java/com/example/shrikrishna/notification/NotificationView.java
@@ -0,0 +1,22 @@
+package com.example.shrikrishna.notification;
+
+/**
+ * Created by Shri Krishna on 10/3/2015.
+ */
+import android.app.Activity;
+        import android.app.NotificationManager;
+        import android.os.Bundle;
+public class NotificationView extends Activity
+        {
+@Override
+public void onCreate(Bundle savedInstanceState)
+        {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+//---look up the notification manager service---
+        NotificationManager nm = (NotificationManager)
+        getSystemService(NOTIFICATION_SERVICE);
+//---cancel the notification that we started---
+        nm.cancel(getIntent().getExtras().getInt("notificationID"));
+        }
+     }
